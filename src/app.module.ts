c9610776f6bc56d12de6edbc3dcd2f3b7ca5e7e0
bkuste2 <igorribeiro632@gmail.com>
2024-09-09 13:38:28 -0300

    🔥 remove: Remove all tests and default controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './entities/auth/auth.module';
 import { UsersModule } from './entities/users/users.module';
 
 @Module({
   imports: [AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
