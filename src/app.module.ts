8b5f51b9bc8fe72d3b3aeb38eb6d658fd58a2c95
Ismael Teixeira da Silva <ismaelteixeiradas1@hotmail.com>
2024-11-23 22:48:55 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './entities/auth/auth.module';
 import { UsersModule } from './entities/users/users.module';
+import { OrdersModule } from './entities/orders/orders.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, OrdersModule],
   controllers: [],
   providers: [],
 })
