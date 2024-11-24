b816ad9b786f175473976b0f885e76dda84bf3ef
bkuste2 <igorribeiro632@gmail.com>
2024-11-24 14:44:27 -0300

    :rocket: feat: Create findByUserId and findAllByRange methods in orders service

--- src/entities/orders/order.controller.ts
+++ src/entities/orders/order.controller.ts
@@ -1,19 +1,23 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Get,
   HttpCode,
   HttpStatus,
   Post,
+  Query,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { OrderStatus } from '@prisma/client';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrdersService } from './orders.service';
+import { stat } from 'fs';
 
 @ApiTags('Orders')
 @Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(private readonly ordersService: OrdersService) { }
 
   @HttpCode(HttpStatus.OK)
   @Post()
@@ -31,5 +35,20 @@ export class OrdersController {
   @Get(':id')
   async findOne(@Body() id: string) {
     return this.ordersService.findOne(id);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('user/:userId')
+  async findByUserId(@Body() userId: string) {
+    return this.ordersService.findByUserId(userId);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('report/range')
+  async findAllByRange(
+    @Query('filter', new DefaultValuePipe("Today")) filter: "Today" | "Week" | "Month" | "Year",
+    @Query('status') status?: OrderStatus,
+  ) {
+    return this.ordersService.findAllByRange(filter, status);
   }
 }
